e7868ec836dbc612707b77397b9dce1c05428ca1
Insup Jung <31408300+jis1218@users.noreply.github.com>
2017-09-06 11:38:38 +0900

    first commit

---
+++ src/AlgoMain.java
@@ -0,0 +1,35 @@
+
+public class AlgoMain {
+
+	public static void main(String[] args) {
+		
+		AlgoMain algo = new AlgoMain();
+		
+		long evenResult = algo.even(100L);
+		long oddResult = algo.odd(4L);
+		System.out.println(evenResult);
+		System.out.println(oddResult);
+		
+		
+	}
+	
+	public long even(long max){
+		
+		long num = max/2; //max가 짝수이든 홀수이든 max의 순번은 max/2로 구할 수가 있음
+		
+		long total = num*(num+1);
+		
+		return total;
+
+	}
+	
+	public long odd(long max){
+		
+		long num = (max+1)/2; //max가 짝수이든 홀수이든 max의 순번은 (max+1)/2로 구할 수가 있음
+		
+		long total = num*num;
+		
+		return total;
+	}
+
+}
